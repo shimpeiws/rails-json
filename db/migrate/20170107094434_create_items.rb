77bd466452d6e4bfcacaddfba9351dbd8b652cf8
shimpeiws <shimpeiws@gmail.com>
2017-01-07 18:45:49 +0900

    Add item

---
+++ db/migrate/20170107094434_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.json :detail
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/items.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :item do
+    title "MyString"
+    detail ""
+  end
+end
