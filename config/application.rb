b623736020a14ef1dfb61956b85bacfe7359bfdf
shimpeiws <shimpeiws@gmail.com>
2017-01-07 11:50:10 +0900

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,26 @@
+require_relative 'boot'
+
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsJson
+  class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec, view_specs: false, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+  end
+end
