5ebbca471f76f5509b50529e54c3edf03a389155
shimpeiws <shimpeiws@gmail.com>
2017-01-08 10:03:21 +0900

    Add validate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  validate :validate_detail
+
+  private
+
+  def validate_detail
+    schema = load_json_schema
+    JSON::Validator.fully_validate(schema, detail, errors_as_objects: true)
+  end
+
+  def load_json_schema
+    case category
+    when 'book_shop' then
+      JSON.dump(YAML.load(File.open('./config/json_schema/book_shop.yml').read))
+    else
+      JSON.dump(YAML.load(File.open('./config/json_schema/default.yml').read))
+    end
+  end
 end
